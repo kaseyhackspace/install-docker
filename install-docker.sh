9195b52ea477f75db7dcc0b4593371a9466d8bd2
Kasey Martin <39894484+kaseyhackspace@users.noreply.github.com>
2019-03-29 20:22:59 +0800

    updated docker-compose to version 1.24.0

--- install-docker.sh
+++ install-docker.sh
@@ -23,7 +23,7 @@ echo adding local user for docker
 sudo usermod -aG docker ${USER}
 id -nG
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 docker --version
